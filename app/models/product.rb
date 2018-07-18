75a19ab66e4d064d1514837caf369c81c2d7f33a
Alexander <a.peredo@expansesoft.com>
2018-07-18 16:12:55 -0400

    fix when there is no image

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/noimage.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   	
   belongs_to :measure_unit
